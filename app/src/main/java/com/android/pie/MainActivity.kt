bc22cbc6fc16d8966c141f5474fea1eabf66e8f9
win7 <737141437@qq.com>
2018-08-08 09:47:04 +0800

    Android 9 终版新功能

---
+++ app/src/main/java/com/android/pie/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.android.pie
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/*
+
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
